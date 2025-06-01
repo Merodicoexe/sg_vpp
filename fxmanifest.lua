7183c743997489cd80e1a5ea936b599925f18dfe
Merodicoexe <hitherl@seznam.cz>
2025-06-01 21:25:13 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+author 'v0'
+description 'Community Service Script with Database Support'
+version '2.0.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'shared/config.lua',
+    'shared/functions.lua',
+    '@es_extended/imports.lua'
+}
+
+client_scripts {
+    'client/*.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/*.lua',
+}
+
+dependencies {
+    'oxmysql'
+}
